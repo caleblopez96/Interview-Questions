1b6edbed92323f1c99b8dd2bd1f67a272a475c5c
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-08-03 19:15:27 -0700

    Add files via upload

---
+++ cheatSheets/array.some.js
@@ -0,0 +1,12 @@
+// array.some() tells you whether any element in your array passes your test. 
+
+
+function checkAge(age) {
+    return age < 18;
+}
+
+const ageArray = [37, 29, 24, 26, 13];
+
+let check = ageArray.some(checkAge);
+
+console.log(check); // true
